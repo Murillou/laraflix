c011db0881330c45f4b713a38c2b8d07cf6d9947
Murillou <murillovinicius13@hotmail.com>
2025-04-09 17:35:19 -0300

    feat(routes): adiciona rotas de login e registro

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('login.index');
-});
+Route::resource('/', \App\Http\Controllers\LoginController::class);
+
+Route::resource('register', \App\Http\Controllers\RegisterController::class)->only('create', 'store');
