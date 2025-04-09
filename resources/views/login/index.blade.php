e7ff783442759a5b378b9345c06c689af8d43eca
Murillou <murillovinicius13@hotmail.com>
2025-04-09 16:59:56 -0300

    feat: criar formulário de login com estilização

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,16 +1,22 @@
 <x-layout title="Login">
-    <form method="post" class="d-flex flex-column justify-content-center">
-        @csrf
-        <div class="form-group">
-            <label class="form-label" for="email">E-mail:</label>
-            <input type="email" class="form-control" name="email" id="email" />
-        </div>
+    <div class="card p-4 shadow" style="width: 100%; max-width: 400px;">
+        <h1 class="card-title text-center">Laraflix</h1>
+        <form method="post" action="">
+            @csrf
+            <div class="form-group">
+                <label class="form-label" style="cursor: pointer" for="email">E-mail:</label>
+                <input type="email" class="form-control" name="email" id="email" />
+            </div>
 
-        <div class="form-group">
-            <label class="form-label" for="password">Senha:</label>
-            <input type="password" class="form-control" name="password" id="password" />
-        </div>
+            <div class="form-group mt-3">
+                <label class="form-label" style="cursor: pointer" for="password">Senha:</label>
+                <input type="password" class="form-control" name="password" id="password" />
+            </div>
 
-        <button class="btn btn-primary mt-3">Login</button>
-    </form>
+            <div class="d-flex justify-content-between mt-4">
+                <button type="submit" class="btn btn-primary">Login</button>
+                <a href="" class="btn btn-secondary">Registrar</a>
+            </div>
+        </form>
+    </div>
 </x-layout>
