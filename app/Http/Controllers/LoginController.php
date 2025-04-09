610a47a881ce343e4f4add38c90ae27af6659b42
Murillou <murillovinicius13@hotmail.com>
2025-04-09 19:37:47 -0300

    feat(auth): adiciona funcionalidades de login e criação de usuário

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,16 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index()
+    public function create()
     {
-        return view('login.index');
+        return view('login.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $credentials = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
 
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->route('profiles.select');
+        }
+
+        return back()->withErrors([
+            'email' => 'As credenciais estão incorretas.'
+        ])->withInput();
     }
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Repositories\ProfileRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class RegisterController extends Controller
 {
     public function create()
     {
         return view('register.create');
+    }
+
+    public function store(Request $request, ProfileRepository $repository)
+    {
+        $user = User::create($request->except('_token'));
+
+        auth()->login($user);
+
+        if (auth()->check()) {
+            return redirect()->route('profiles.select');
+        } else {
+            return redirect()->back();
+        }
     }
 }
