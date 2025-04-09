12308f9a9bc21774d1afe92bb37dd6cb7e749634
Murillou <murillovinicius13@hotmail.com>
2025-04-09 19:44:11 -0300

    refactor(routes): ajusta rotas de login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/', \App\Http\Controllers\LoginController::class);
+Route::get('/', [\App\Http\Controllers\LoginController::class, 'create'])->name('login.create');
+Route::post('/', [\App\Http\Controllers\LoginController::class, 'store'])->name('login.store');
 
 Route::resource('register', \App\Http\Controllers\RegisterController::class)->only('create', 'store');
+
+Route::resource('profiles', \App\Http\Controllers\ProfileController::class)->only('index');
+Route::get('/profiles/select', [\App\Http\Controllers\ProfileController::class, 'select'])->name('profiles.select');
