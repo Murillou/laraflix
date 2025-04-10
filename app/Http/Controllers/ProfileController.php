1287e0af62b5b3ee011ec0e9aa50e92b35d29c85
Murillou <murillovinicius13@hotmail.com>
2025-04-10 18:42:43 -0300

    feat(profile): cria model profile e adiciona métodos create e store para criação de perfis

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Profile;
+use App\Repositories\ProfileRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,5 +18,28 @@ class ProfileController extends Controller
         $profiles = Profile::where('user_id', Auth::id())->get();
 
         return view('profiles.select', compact('profiles'));
+    }
+
+    public function create()
+    {
+        return view('profiles.create');
+    }
+
+    public function store(Request $request, ProfileRepository $repository)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:15'],
+            'image_url' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+        ]);
+
+        if ($request->hasFile('image_url')) {
+            $data['image_url'] = $request->file('image_url')->store('images', 'public');
+        }
+
+        $data['user_id'] = Auth::id();
+
+        Profile::create($data);
+
+        return redirect()->route('profiles.select')->with('success', 'Perfil criado com sucesso!');
     }
 }
