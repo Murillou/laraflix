164a4a61b9c4bb032866ddaed548684c9d7038e0
Murillou <murillovinicius13@hotmail.com>
2025-04-11 18:38:17 -0300

    refactor(routes): organiza imports e melhora legibilidade

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\LoginController::class, 'create'])->name('login');
-Route::post('/', [\App\Http\Controllers\LoginController::class, 'store'])->name('login.store');
-Route::delete('/', [\App\Http\Controllers\LoginController::class, 'logout'])->name('login.logout');
+Route::get('/', [LoginController::class, 'create'])->name('login');
+Route::post('/', [LoginController::class, 'store'])->name('login.store');
+Route::delete('/', [LoginController::class, 'logout'])->name('login.logout');
 
-Route::resource('register', \App\Http\Controllers\RegisterController::class)->only('create', 'store');
+Route::resource('register', RegisterController::class)->only('create', 'store');
 
-Route::get('/profiles/select', [\App\Http\Controllers\ProfileController::class, 'select'])->name('profiles.select');
-Route::resource('profiles', \App\Http\Controllers\ProfileController::class);
+Route::get('/profiles/select', [ProfileController::class, 'select'])->name('profiles.select');
+Route::resource('profiles', ProfileController::class);
