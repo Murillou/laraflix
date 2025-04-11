d236b8442268fb7daa3739b7e66a17d45ef835fd
Murillou <murillovinicius13@hotmail.com>
2025-04-11 19:12:05 -0300

    feat(profile): adiciona novos métodos aos ProfileRepository

--- app/Repositories/ProfileRepository.php
+++ app/Repositories/ProfileRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\Profile;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileRepository implements ProfileRepositoryInterface
 {
@@ -13,7 +14,23 @@ class ProfileRepository implements ProfileRepositoryInterface
 
     public function create(array $data)
     {
+        $userId = $data['user_id'] ?? Auth::id();
+
+        $count = Profile::where('user_id', $userId)->count();
+
+        if ($count >= 5) {
+            abort(403, 'Você só pode ter no máximo 5 perfis.');
+        }
+
         return Profile::create($data);
     }
+    public function update(array $data, Profile $profile)
+    {
+        $profile->update($data);
+    }
 
+    public function delete(Profile $profile)
+    {
+        $profile->delete();
+    }
 }
