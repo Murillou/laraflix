b9db0473fae9803de1cb8812c5f8185eacf64a9e
Murillou <murillovinicius13@hotmail.com>
2025-04-11 17:59:23 -0300

    feat(view): adiciona exibição de erros de validação no login

--- resources/views/login/create.blade.php
+++ resources/views/login/create.blade.php
@@ -5,21 +5,26 @@
             @csrf
             <div class="form-group">
                 <label class="form-label" style="cursor: pointer" for="email">E-mail:</label>
-                <input type="email" class="form-control" name="email" id="email" />
+                <input type="email" class="form-control @error('email') is-invalid @enderror "  name="email" id="email" value="{{old('email')}}"
+                       required autocomplete="email" />
+                @error('email')
+                    <div class="invalid-feedback"> {{ $message }}</div>
+                @enderror
             </div>
 
             <div class="form-group mt-3">
                 <label class="form-label" style="cursor: pointer" for="password">Senha:</label>
-                <input type="password" class="form-control" name="password" id="password" />
+                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password"
+                       required autocomplete="current-password" />
+                @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
-
-            <x-error></x-error>
 
             <div class="d-flex justify-content-between mt-4">
                 <button type="submit" class="btn btn-primary">Login</button>
                 <a href="{{ route('register.create') }}" class="btn btn-secondary">Registrar</a>
             </div>
-
         </form>
     </div>
 </x-layout>
