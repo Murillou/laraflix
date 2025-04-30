e60181a390fb80ca3c600d7f80a5594cb2d4c492
Murillou <murillovinicius13@hotmail.com>
2025-04-30 17:09:20 -0300

    feat(movie): adiciona rotas para filme favorito e filme único e relaciona tabelas na model

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -7,19 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $fillable = ['name', 'image_url', 'user_id'];
-
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    public function favorites()
+    public function movies()
     {
-        return $this->belongsToMany(Movie::class, 'favorites');
+        return $this->belongsToMany(Movie::class, 'profile_movie')
+            ->withPivot('status', 'scheduled_at')
+            ->withTimestamps();
     }
 
-    public function watchlists()
+    public function favoriteMovies()
     {
-        return $this->hasMany(WatchList::class);
+        return $this->movies()->wherePivot('status', 'favorite');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -13,3 +14,8 @@ Route::resource('register', RegisterController::class)->only('create', 'store');
 
 Route::get('/profiles/select', [ProfileController::class, 'select'])->name('profiles.select');
 Route::resource('profiles', ProfileController::class);
+
+Route::get('/profiles/{profile}/movies', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/profiles/{profile}/movies/{movieId}', [MovieController::class, 'show'])->name('movies.show');
+Route::post('/profiles/{profile}/movies/{movieId}/favorite', [MovieController::class, 'addToFavorites'])->name('movies.favorite');
+Route::get('/profiles/{profile}/favorites', [MovieController::class, 'favorites'])->name('movies.favorites');
